3cbfb45a9079574b8c5641003c1f56278d99133e
dindadwiulfahtunnisa <dindadwiulfahh@gmail.com>
2022-02-17 06:59:53 +0700

    edit home

--- layouts/pages/home.php
+++ layouts/pages/home.php
@@ -6,6 +6,8 @@
             <div class="row gx-5 justify-content-center">
                 <div class="col-lg-6">
                     <div class="text-center my-5">
+                        <h2 class="display-5 fw-bolder text-white mb-2">APLIKASI SALES FORECASTING</h2>
+                        <br>
                         <h1 class="display-5 fw-bolder text-white mb-2">Apotek Ruhul J</h1>
                         <p class="lead text-white-50 mb-4">Jln Pagambiran No.10B</p>
                         <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
